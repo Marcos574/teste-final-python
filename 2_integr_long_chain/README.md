d7505fcaa575550cdad415ff9a4f6971074ad5bb
Marcos574 <mvdeus574@gmail.com>
2025-01-01 17:59:47 -0300

    Finalizando questão 2

---
+++ 2_integr_long_chain/README.md
@@ -0,0 +1,60 @@
+# Questão 2 - Integração com langchain
+
+## Instrução
+
+(Curso de LangChain: Prioridade Alta) 
+Utilizando a biblioteca LangChain, crie uma aplicação simples que: 
+
+• Configure um modelo de linguagem para responder a perguntas sobre um texto 
+fornecido pelo usuário.
+• Receba como entrada um texto qualquer digitado pelo usuário. 
+• Permita ao usuário fazer perguntas sobre o texto e exiba as respostas geradas pelo 
+modelo. 
+
+Obs.: Documente as etapas necessárias para configurar o ambiente e instalar as 
+dependências. 
+
+## Requisitos
+
+- Python 3.x instalado
+- Bibliotecas Python:
+    - dotenv: Para carregar variáveis de ambiente a partir de um arquivo .env
+    - langchain_openai: Para interagir com a API do modelo de linguagem.
+
+## Passos para configurar o ambiente
+
+1 - Instalar as dependências necessárias
+
+Na raiz do projeto rode
+
+```
+pip install -r requirements.txt
+```
+
+2 - Criar o arquivo .env na raiz do repositório
+
+O programa utiliza a variável de ambiente TOGETHER_API_KEY para configurar o acesso à API da Together. Para isso, crie um arquivo .env na raiz do seu projeto e insira sua chave de API:
+
+```
+TOGETHER_API_KEY=SuaChaveAPIaqui
+```
+
+## Como usar
+
+1 - Preparação do arquivo texto:
+    O arquivo user_txt.txt contém o texto que o modelo usará como base para responder as perguntas, modifique como desejar
+
+2 - Executando o programa:
+
+- Abra um terminal nessa pasta
+
+```
+python main.py
+```
+- Digite a pergunta que desejar no terminal
+
+## Exemplo
+
+![questão 2](../exemplos/questao-2.gif)
+
+
--- 2_integr_long_chain/main.py
+++ 2_integr_long_chain/main.py
@@ -1,22 +1,61 @@
-import getpass
 import os
-
-if not os.environ.get("TOGETHER_API_KEY"):
-  os.environ["TOGETHER_API_KEY"] = getpass.getpass("Enter API key for Together AI: ")
-
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-model = ChatOpenAI(
-    base_url="https://api.together.xyz/v1",
-    api_key=os.environ["TOGETHER_API_KEY"],
-    model="mistralai/Mixtral-8x7B-Instruct-v0.1",
-)
+def config_ai_model():
+    load_dotenv()
+    global together_api_key, model
 
-print(model.invoke("Hello, world!"))
+    together_api_key = os.getenv("TOGETHER_API_KEY")
+
+    model = ChatOpenAI(
+        base_url="https://api.together.xyz/v1",
+        api_key=together_api_key,
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    )
+
+def ai_message_to_dict(ai_message):
+    """
+    Converte um objeto AIMessage para um dicionário serializável.
+    """
+
+    if hasattr(ai_message, 'text'):
+        return {'text': ai_message.text}
+    
+    return vars(ai_message)
+
+def make_prompt(question):
+
+    with open('user_text.txt', 'r', encoding='utf-8') as file:
+        user_text = file.read()
+
+    prompt = {"context": user_text, "question": question}
+
+    prompt_string = f"Contexto: {prompt['context']} \n\nPergunta: {prompt['question']}"
+
+    return prompt_string
+
+
+def ai_interation():
+
+    question = input("Digite a sua pergunta sobre o texto: \n")
+    prompt = make_prompt(question)
+    data = model.invoke(prompt)
+
+    if isinstance(data, list):
+        data = [ai_message_to_dict(item) for item in data]
+    else:
+        data = ai_message_to_dict(data)
+
+    print("\nResposta do modelo: \n")
+    print(data["content"])
+
 
 def main():
 
-    pass
+    config_ai_model()
+    ai_interation()
+
 
 if __name__ == '__main__':
     main()
